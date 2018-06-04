cb6e8e80efd4aaa6f017d65ae7eaa6bbd3eb78cc
Vasile Pește <vasile.alexandru.peste@protonmail.ch>
2018-06-04 09:52:45 +0200

    NPM module moved to its base path.

--- src/drivers/npm/driver.js
+++ src/drivers/npm/driver.js
@@ -19,16 +19,21 @@ class Driver {
       maxDepth: 3,
       maxUrls: 10,
       maxWait: 5000,
+			proxy: null,
       recursive: false,
       userAgent: 'Mozilla/5.0 (compatible; Wappalyzer)',
+      htmlMaxCols: 2000,
+      htmlMaxRows: 3000,
     }, options || {});
 
-    this.options.debug = Boolean(this.options.debug);
+    this.options.debug = Boolean(+this.options.debug);
+    this.options.recursive = Boolean(+this.options.recursive);
     this.options.delay = this.options.recursive ? parseInt(this.options.delay, 10) : 0;
     this.options.maxDepth = parseInt(this.options.maxDepth, 10);
     this.options.maxUrls = parseInt(this.options.maxUrls, 10);
     this.options.maxWait = parseInt(this.options.maxWait, 10);
-    this.options.recursive = Boolean(this.options.recursive);
+    this.options.htmlMaxCols = parseInt(this.options.htmlMaxCols, 10);
+    this.options.htmlMaxRows = parseInt(this.options.htmlMaxRows, 10);
 
     this.origPageUrl = url.parse(pageUrl);
     this.analyzedPageUrls = [];
@@ -44,12 +49,6 @@ class Driver {
 
     this.wappalyzer.driver.log = (message, source, type) => this.log(message, source, type);
     this.wappalyzer.driver.displayApps = (detected, meta, context) => this.displayApps(detected, meta, context);
-    
-    this.browser = new Browser({
-      silent: true,
-      userAgent: this.options.userAgent,
-      waitDuration: this.options.maxWait,
-    });
 
     process.on('uncaughtException', e => this.wappalyzer.log('Uncaught exception: ' + e.message, 'driver', 'error'));
   }
@@ -114,8 +113,14 @@ class Driver {
   }
 
   visit(pageUrl, timerScope, resolve) {
-    const browser = this.browser;
-    
+    const browser = new Browser({
+			proxy: this.options.proxy,
+      silent: true,
+      strictSSL: false,
+      userAgent: this.options.userAgent,
+      waitDuration: this.options.maxWait,
+    });
+
     this.timer('browser.visit start; url: ' + pageUrl.href, timerScope);
 
     browser.visit(pageUrl.href, () => {
@@ -141,7 +146,7 @@ class Driver {
         .then(() => {
           const links = Array.prototype.reduce.call(
             browser.document.getElementsByTagName('a'), (results, link) => {
-              if ( link.protocol.match(/https?:/) || link.hostname === this.origPageUrl.hostname || extensions.test(link.pathname) ) {
+              if ( link.protocol.match(/https?:/) && link.hostname === this.origPageUrl.hostname && extensions.test(link.pathname) ) {
                 link.hash = '';
 
                 results.push(url.parse(link.href));
@@ -217,11 +222,11 @@ class Driver {
     let html = '';
 
     try {
-      html = browser.html();
-
-      if ( html.length > 50000 ) {
-        html = html.substring(0, 25000) + html.substring(html.length - 25000, html.length);
-      }
+      html = browser.html()
+        .split('\n')
+        .slice(0, this.options.htmlMaxRows / 2).concat(html.slice(html.length - this.options.htmlMaxRows / 2))
+        .map(line => line.substring(0, this.options.htmlMaxCols))
+        .join('\n');
     } catch ( error ) {
       this.wappalyzer.log(error.message, 'browser', 'error');
     }
@@ -229,29 +234,20 @@ class Driver {
     return html;
   }
 
-  getScripts(browser, window = null) {
-    let document;
-      
-    if (window === null) {
-        document = browser.document;
-    }
-    else {
-        document = window.document;
-    }
-    
-    if ( !document || !document.scripts ) {
+  getScripts(browser) {
+    if ( !browser.document || !browser.document.scripts ) {
       return [];
     }
 
     const scripts = Array.prototype.slice
-      .apply(document.scripts)
+      .apply(browser.document.scripts)
       .filter(script => script.src)
       .map(script => script.src);
 
     return scripts;
   }
 
-  getJs(browser, window = null) {
+  getJs(browser) {
     const patterns = this.wappalyzer.jsPatterns;
     const js = {};
 
@@ -266,7 +262,7 @@ class Driver {
 
           let value = properties.reduce((parent, property) => {
             return parent && parent.hasOwnProperty(property) ? parent[property] : null;
-          }, window || browser.window);
+          }, browser.window);
 
           value = typeof value === 'string' || typeof value === 'number' ? value : !!value;
 
@@ -295,14 +291,14 @@ class Driver {
     return cookies;
   }
 
-  crawl(pageUrl, index, depth = 1) {
+  crawl(pageUrl, index = 1, depth = 1) {
     pageUrl.canonical = pageUrl.protocol + '//' + pageUrl.host + pageUrl.pathname;
 
     return new Promise(resolve => {
       this.fetch(pageUrl, index, depth)
         .catch(() => {})
         .then(links => {
-          if ( links && Boolean(this.options.recursive) && depth < this.options.maxDepth ) {
+          if ( links && this.options.recursive && depth < this.options.maxDepth ) {
             return this.chunk(links.slice(0, this.options.maxUrls), depth + 1);
           } else {
             return Promise.resolve();
